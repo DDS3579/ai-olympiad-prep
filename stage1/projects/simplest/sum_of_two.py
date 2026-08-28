2cf8e1b55a9b0f8027e03d95be94f9b1b4556630
Divya Darsheel Sharma <divyadarsheel.dds@gmail.com>
2026-08-28 11:34:51 +0545

    added the most bginner project

---
+++ stage1/projects/simplest/sum_of_two.py
@@ -0,0 +1,10 @@
+# Program to input 2 numbers and Print their sum
+
+def sum_of_two_numbers(num1, num2):
+    return num1 + num2
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = sum_of_two_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is: {result}")
